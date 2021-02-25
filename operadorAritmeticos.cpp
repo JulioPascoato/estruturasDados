0b2f2aac1693a0a736b04058bd647048daaeadac
jpascoato <jpascoato@gmail.com>
2021-02-25 19:52:32 -0300

    Primeiro Commit

---
+++ operadorAritmeticos.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int number1;
+int number2;
+
+int main(){
+
+    std::cout << "Digite o primeiro número: ";
+    std::cin >> number1;
+    std::cout << "Digite o segundo número: ";
+    std::cin >> number2;
+
+    int sum = number1 + number2;
+    int sub = number1 - number2;
+    int mult = number1 * number2;
+    int div = number1 / number2;
+
+    std::cout << "Soma: " << sum << "\n";
+    std::cout << "Subtração: " << sub << "\n";
+    std::cout << "Multiplicação: " << mult << "\n";
+    std::cout << "Divisão: " << div << "\n";
+
+}
